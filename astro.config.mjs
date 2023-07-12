cba4664783c7374e0102d433d8e6b1ca132a9691
Martin Prinz <martin.prinz@maibornwolff.de>
2023-07-12 13:06:59 +0000

    feat(vercel): add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), preact()],
-  adapter: cloudflare()
+  adapter: vercel()
 });
