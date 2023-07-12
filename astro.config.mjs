20408f8346656cdfbea59c7d5bdd47a84a96da14
Martin Prinz <martin.prinz@maibornwolff.de>
2023-07-12 13:10:44 +0000

    feat(vercel): use SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [tailwind(), preact()],
   adapter: vercel()
 });
